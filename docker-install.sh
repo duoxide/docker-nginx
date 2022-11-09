30d3af89af1296d2bb2cc9bff2b10d334bca7b7d
duoxide <zauchka007@gmail.com>
2022-11-09 11:43:51 +0200

    ready build

---
+++ docker-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt update
+apt install ca-certificates curl gnupg lsb-release -y
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose -y
+groupadd docker
+usermod -aG docker ubuntu
+sleep 1m
+docker run -d -p 80:80 nginx
