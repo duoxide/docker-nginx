0c82f3c885bebdc2ed0d2e04b2e623b3d90fd690
duoxide <zauchka007@gmail.com>
2022-11-22 09:51:32 +0200

    added comments in bash script

--- docker-install.sh
+++ docker-install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 apt update
 apt install ca-certificates curl gnupg lsb-release -y
+# Add Docker’s official GPG key
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Set up the repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-compose -y
